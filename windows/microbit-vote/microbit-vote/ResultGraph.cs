a99f566140274736c93cb1c9030f054799685640
Joe Finney <joe@comp.lancs.ac.uk>
2016-06-26 13:56:38 +0100

    microbit-quiz: Added Windows "microbit-vote" App

---
+++ windows/microbit-vote/microbit-vote/ResultGraph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace microbit_vote
+{
+    public partial class ResultGraph : Form
+    {
+        List<Point> bars = new List<Point>();
+
+        public ResultGraph(List<Vote> votes, List<string> options, string title)
+        {
+            InitializeComponent();
+
+            this.questionTextBox.Text = title;
+
+            this.resultsChart.Series.Clear();
+
+            for (int i=0; i<options.Count; i++)
+            {
+                int value = 0;
+                foreach (Vote v in votes)
+                    if (v.choice == i)
+                        value++;
+
+                string s = options[i];    
+                this.resultsChart.Series.Add(s);
+                DataPoint d = new DataPoint();
+                d.SetValueY(value);
+                this.resultsChart.Series[s].Points.Add(d);
+            }
+        }
+    }
+}
